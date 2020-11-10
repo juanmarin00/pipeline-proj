f57d3886946705560f90c0c3b8f65416605a395e
Juan Marin <juan.m.c.2000@gmail.com>
2020-11-10 16:21:42 +0100

    finished project

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ I used data from basketball-reference and scraped hoops-hype website to get play
 * Repo description: 
 In this repository you can find the notebooks used to clean, analyse and visualise data in the notebooks folder, graphs in the praphs folder and all the data used in separate and combined csvs can be found in the data folder.
 #
-* main.py (work in progress):
-Objective: to let users query our dataset to find stats for a specific player (already working) and the leaders of any desired stat
+* main.py:
+When running this program users will be able to query the dataset to find stats for any player or the n leaders for any stat.
 #
 * Results:
 To help present our discoveries I created a presentation on Google Slides which can be found here: https://docs.google.com/presentation/d/1IT8aTbuES9KD3V7E7jjdwrPdU82nBhBZ7wLgNNXYXWY/edit#slide=id.ga8acce97da_1_44
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pandas as pd
 df = pd.read_csv (r'/Users/juanmarin/IronHack/pipeline-proj/data/ordered.csv')
 
+def query_identifier(query_word_list):
+    #Will get a string of a query and will decide what type of query user wants
+    #wether stat leaders or player specific stats, and then call corresponding function
+    if query_word_list[0] == "stat":
+        query_word_list.pop(0)
+        retrieve_stats(query_word_list)
+    elif query_word_list[0] == "player":
+        query_word_list.pop(0)
+        retrieve_player_stats(query_word_list)
+
 
 def retrieve_player_stats(query_word_list):
+    #return the corresponding dataframe row for the wanted player
     playername = query_word_list[0] + " " + query_word_list[-1]
     print(playername)
     try:
@@ -12,26 +23,22 @@ def retrieve_player_stats(query_word_list):
 
 
 def retrieve_stats(query_word_list):
+    #will break down input array into desired order, stat and number of players
+    #then return corresponding rows from dataframe
     order = query_word_list[0] == "worst"
-    number_of_stats = query_word_list[1]
+    number_of_stats = int(query_word_list[1])
     stat = query_word_list[2]
     print(order, number_of_stats, stat)
 
-    try:##modify this
-        print(df.sort_values(by=stat, ascending = order).head(number_of_stats))
-        return df.sort_values(by=df[stat], ascending = order).head(number_of_stats)##order by stat (cant put quote marks)
+    try:
+        print(df.sort_values(by=[f"{stat}"], ascending = order).head(number_of_stats))
+        return df.sort_values(by=[f"{stat}"], ascending = order).head(number_of_stats)##order by stat (cant put quote marks)
     except:
         print("Sorry couldnt find a result for your query, please try again")
 
 
-def query_identifier(query_word_list):
-    if query_word_list[0] == "stat":
-        query_word_list.pop(0)
-        retrieve_stats(query_word_list)
-    elif query_word_list[0] == "player":
-        query_word_list.pop(0)
-        retrieve_player_stats(query_word_list)
-    
+
+
 
 
 
@@ -42,8 +49,19 @@ print("You can either query a player by their full name by indicating the keywor
 query = str(input("Please enter your first query: "))
 query_list = query.split(" ")
 query_identifier(query_list)
+#check if they still want to query dataset
+want_more_query = str(input("Would you like to query more? (y/n): "))
 
+#Get more querys and run functions with new queries
+while want_more_query == "y":
+    query = str(input("Please enter your new query: "))
+    query_list = query.split(" ")
+    query_identifier(query_list)
+    want_more_query = str(input("Would you like to query more? (y/n): "))
 
+#goodbye message
+if want_more_query == "n":
+    print("Hope you enjoyed using this program.\nBye!")
 
 
 
